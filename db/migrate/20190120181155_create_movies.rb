8aa590313ee3591c013232d50648e417fbc67ee4
OscarB <oballesteros@abstract.cl>
2019-01-20 15:13:40 -0300

    all rspect test for Category

---
+++ db/migrate/20190120181155_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.string :description
+      t.string :year
+      t.string :ranking
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
